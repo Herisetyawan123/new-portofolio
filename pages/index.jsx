106ad5508e7230a17f6239989f1d7e60fae90776
Heri Setyawan <herisetyawan233@gmail.com>
2023-01-01 22:30:25 +0700

    preapare meta website

---
+++ pages/index.jsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Heri Setyawan | Portofolio</title>
+        <meta name="keywords" content="heri setyawan, herisetyawan, @herisetyawan, #herisetyawan, unej, universitas jember, fullstack, backend engineering, frontend, informatika, portofolio, sutipan" />
+        <meta name="description" content="Perkenalkan namaku Heri setyawan, saya adalah seorang fullstack developer dan juga seorang mahasiswa." />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=7" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta property="og:image" content="http://www.example.com/image01.jpg"></meta>
+        <link rel="icon" type="image/png" sizes="192x192"  href="/ico/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/ico/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/ico/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/ico/favicon-16x16.png" />
+        <link rel="manifest" href="/ico/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <main>
+
+
+      </main>
+    </div>
+  );
+}
