5b3d6143550d899df21c0c382c40c9266f98a1f0
Heri Setyawan <herisetyawan233@gmail.com>
2023-01-04 08:47:15 +0700

    fix: profile error responsive

--- components/parts/Profile.jsx
+++ components/parts/Profile.jsx
@@ -17,13 +17,13 @@ import ReactI from "../../assets/icon/reacti.png"
 
 export default function Profile(){
   return (
-    <Container maxW='6xl' px={10} className="mt-24 sm:mt-40">
-      <Flex className="flex-col-reverse sm:flex-row justify-between items-center" wrap={'wrap'}>
-        <Box>
+    <Container maxW='6xl' className="mt-24 sm:mt-40 px-10">
+      <Flex className="flex-col-reverse sm:flex-row gap-y-20 justify-between items-center">
+        <Box className={'w-full'}>
           <span className="bg-[var(--purple-10)] px-3 py-1 rounded-xl text-[var(--purple)]">
             👋 Selamat datang !
           </span>
-          <h1 className="font-extrabold text-6xl mt-7 w-96">Heri Setyawan</h1>
+          <h1 className="font-extrabold text-3xl md:text-6xl mt-7 w-96">Heri Setyawan</h1>
           <p className="mt-4 text-xl opacity-50">Fullstack developer · Back-end Engineering</p>
           <HStack className="mt-7" spacing={"30px"}>
             <Link href={"https://www.linkedin.com/in/heri-setyawan-905758202/"}>
@@ -36,7 +36,7 @@ export default function Profile(){
               <Image src={Instagram} alt="Linked In | Heri Setyawan" />
             </Link>
           </HStack>
-          <Box className="mt-10 sm:hidden flex gap-x-5 items-center">
+          <Box className="mt-10 sm:hidden flex gap-x-5 items-center flex-wrap">
             <Link href={"/"} className="bg-transparent text-[var(--purple)]" bg="transparent">
               <span className="px-4 py-2 rounded-lg 
                flex items-center gap-x-2 hover:text-[var(--dark)] hover:bg-[var(--purple)] duration-500">
@@ -52,13 +52,13 @@ export default function Profile(){
             </Link>
           </Box>
         </Box>
-        <div className="block md:flex items-center">
-          <div className="relative flex justify-center w-[400px]">
-            <Image src={Elipse} className="transition-all duration-1000 animate-pulse" alt="purple elipse" />
+        <div className="flex justify-center items-center w-full">
+          <div className="relative flex justify-center">
+            <Image src={Elipse} className="transition-all w-full duration-1000 animate-pulse" alt="purple elipse" />
             <Image src={AvatarMe} className="absolute top-16 w-60 md:w-96" alt="purple elipse" />
-            <Image src={Golang} className="absolute transition-all duration-1000 animate-bounce bottom-5 -left-10" alt="Golang Language" />
-            <Image src={TypeScript} className="absolute transition-all duration-1000 animate-pulse animatite-pulse top-10 right-5" alt="TypeScript Language" />
-            <Image src={ReactI} className="absolute transition-all duration-100 animate-spin animatite-pulse top-3 -left-5" alt="React Language" />
+            <Image src={Golang} className="w-32 lg:h-32 absolute transition-all duration-1000 animate-bounce bottom-0 -left-10 sm:bottom-5 md:left-5 md:bottom-10" alt="Golang Language" />
+            <Image src={TypeScript} className="w-32 lg:h-32 absolute transition-all duration-1000 animate-pulse animatite-pulse top-10 right-5" alt="TypeScript Language" />
+            <Image src={ReactI} className="w-32 lg:h-32 absolute transition-all duration-100 animate-spin animatite-pulse top-3 -left-5 md:left-10 md:top-10" alt="React Language" />
           </div>
           <Box className="hidden md:flex items-center flex-col">
             <Link href={"/"} className="bg-transparent text-[var(--purple)]" bg="transparent">
